3c4b9f484ed75f5d4b2f7f7c2e1434467c0a51ba
filipkof <71509204+svegoe@users.noreply.github.com>
2024-08-13 23:22:15 +0300

    update version addToCard

--- src/test/java/tests/TestsChitaiGorod.java
+++ src/test/java/tests/TestsChitaiGorod.java
@@ -1,13 +1,18 @@
 package tests;
 
+import com.codeborne.selenide.SelenideElement;
+import dev.failsafe.internal.util.Assert;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.junit.jupiter.api.Assertions;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestsChitaiGorod extends TestBase {
  @Test
@@ -17,8 +22,10 @@ public class TestsChitaiGorod extends TestBase {
      open("https://www.chitai-gorod.ru/");
      $x("//*[@name='phrase']").setValue("Test");
  }
+
  @Test
- void addToCard () {
+ // Тест кейс на добавление одной книги в корзину и сравнение цены книги и итоговой суммы
+ public void addToCard() {
   open("https://www.chitai-gorod.ru/");
   $(".header-search__input").setValue("Анна Каренина").pressEnter();
   $(".cookie-notice").shouldBe(visible);
@@ -27,10 +34,12 @@ public class TestsChitaiGorod extends TestBase {
   executeJavaScript("document.getElementsByClassName('popmechanic-js-wrapper')[0].style.display = 'none'");
   $$(".products-list").first()
           .$(".product-buttons")
-          .$(".button").click();
+          .$(".button").hover().click();
   $(".header-cart").click();
+  String FirstPrice = $(".product-price__value").getText();
+  String SummaryPrice = $(".cart-sidebar__info-summary").$(".info-item__value").getText();
+  assertEquals(FirstPrice, SummaryPrice);
  }
-
 
  @Test
  void turnToPageTwo () {
@@ -43,4 +52,3 @@ public class TestsChitaiGorod extends TestBase {
    sleep(3000);
  }
 }
-
